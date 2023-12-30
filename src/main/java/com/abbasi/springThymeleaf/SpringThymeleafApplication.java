fb2a50cf6876287b3e5a1c918dd66801cb2fa79b
mohammad Abbasi <mohammed@virtugrp.com>
2023-12-30 18:36:43 +0330

    home controller and html page was added

---
+++ src/main/java/com/abbasi/springThymeleaf/SpringThymeleafApplication.java
@@ -0,0 +1,18 @@
+package com.abbasi.springThymeleaf;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("test");
+	}
+}
