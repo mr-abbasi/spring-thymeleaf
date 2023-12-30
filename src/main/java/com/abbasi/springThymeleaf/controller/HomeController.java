d31d3619d49c97716222f2eb3a8529e4cd86c400
mohammad Abbasi <mohammed@virtugrp.com>
2023-12-30 19:25:31 +0330

    changing home page to dynamically load the content from the controller

--- src/main/java/com/abbasi/springThymeleaf/controller/HomeController.java
+++ src/main/java/com/abbasi/springThymeleaf/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.abbasi.springThymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping(value = {"/","","/home"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("userName","John Doe");
         return "home";
     }
 }
