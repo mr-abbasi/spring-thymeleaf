8ccb765c60892655e683898b254028e11042b423
mohammad Abbasi <mohammed@virtugrp.com>
2024-01-04 20:01:21 +0330

    spring validation is added to the project

--- src/main/java/com/abbasi/springThymeleaf/controller/ContactUsController.java
+++ src/main/java/com/abbasi/springThymeleaf/controller/ContactUsController.java
@@ -2,8 +2,13 @@ package com.abbasi.springThymeleaf.controller;
 
 import com.abbasi.springThymeleaf.model.Contact;
 import com.abbasi.springThymeleaf.service.ContactService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -15,12 +20,16 @@ public class ContactUsController {
     }
 
     @GetMapping("/contactus")
-    public String contactUs() {
+    public String contactUs(Model model) {
+        model.addAttribute("contact",new Contact());
         return "contactus";
     }
 
     @PostMapping("/saveContactUs")
-    public String saveContactUs(Contact contact) {
+    public String saveContactUs(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if(errors.hasErrors()){
+            return "contactus";
+        }
         contactService.addContact(contact);
 
         return "index";
--- src/main/java/com/abbasi/springThymeleaf/model/Contact.java
+++ src/main/java/com/abbasi/springThymeleaf/model/Contact.java
@@ -1,12 +1,25 @@
 package com.abbasi.springThymeleaf.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class Contact {
+    @NotEmpty(message = "Full Name is required")
+    @Size(min = 3,max = 40,message = "Full Name must be between 3 and 40")
+    @Pattern(regexp = "[a-zA-Z ]+",message = "Only alphabetic chars are allowed for Full Name")
     private String fullName;
+
+    @Email(message = "Email isn't correct")
+    @NotBlank(message = "Email is Required")
     private String email;
+
+    @NotBlank(message = "Subject is required")
+    @Size(min=3,max = 50,message = "Subject must be between 3 and 50")
     private String subject;
+
+    @NotBlank
+    @Size(min=10,max = 200,message = "Message must be between 10 and 200")
     private String message;
 }
